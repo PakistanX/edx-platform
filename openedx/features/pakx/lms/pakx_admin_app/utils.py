2f7f08c382b0c1590ed5537694fb8ebd07dcc6d9
Faizan Shah <faizan.shah@edly.io>
2021-06-22 14:10:08 +0500

    PKX-244 User Registration API

--- openedx/features/pakx/lms/pakx_admin_app/utils.py
+++ openedx/features/pakx/lms/pakx_admin_app/utils.py
@@ -10,13 +10,12 @@ from django.urls import reverse
 from django.utils.http import int_to_base36
 from edx_ace import ace
 from edx_ace.recipient import Recipient
+from organizations.models import Organization
 
 from openedx.core.djangoapps.ace_common.template_context import get_base_template_context
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
-from organizations.models import Organization
 
-
-from .constants import GROUP_TRAINING_MANAGERS, ORG_ADMIN, STAFF, TRAINING_MANAGER, LEARNER, GROUP_ORGANIZATION_ADMIN
+from .constants import GROUP_TRAINING_MANAGERS, ORG_ADMIN, TRAINING_MANAGER, LEARNER, GROUP_ORGANIZATION_ADMIN
 from .message_types import RegistrationNotification
 
 
--- openedx/features/pakx/lms/pakx_admin_app/views.py
+++ openedx/features/pakx/lms/pakx_admin_app/views.py
@@ -5,6 +5,7 @@ import uuid
 from django.contrib.auth.models import Group, User
 from django.db import transaction
 from django.db.models import F, Prefetch
+from organizations.models import Organization
 from rest_framework import generics, status, views, viewsets
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.filters import OrderingFilter
@@ -14,13 +15,13 @@ from six import text_type
 from lms.djangoapps.grades.api import CourseGradeFactory
 from openedx.features.pakx.lms.overrides.utils import get_course_progress_percentage
 from student.models import CourseEnrollment
-from organizations.models import Organization
 
 from .utils import get_roles_q_filters, specify_user_role, send_registration_email
 from .constants import GROUP_ORGANIZATION_ADMIN, GROUP_TRAINING_MANAGERS
 from .pagination import CourseEnrollmentPagination, PakxAdminAppPagination
 from .permissions import CanAccessPakXAdminPanel
-from .serializers import UserSerializer, BasicUserSerializer, UserProfileSerializer, LearnersSerializer, UserCourseEnrollmentSerializer
+from .serializers import UserSerializer, BasicUserSerializer, UserProfileSerializer,\
+    LearnersSerializer, UserCourseEnrollmentSerializer
 from .utils import get_learners_filter, get_user_org_filter
 
 
@@ -77,7 +78,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
                     user_profile = profile_serializer.save()
                     specify_user_role(user, role)
                     send_registration_email(user, user_profile, request.scheme)
-                    return Response(self.get_serializer(self.get_queryset().filter(id=user.id).first()).data, status=status.HTTP_200_OK)
+                    return Response(
+                        self.get_serializer(
+                            self.get_queryset().filter(id=user.id).first()).data, status=status.HTTP_200_OK)
             return Response(profile_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
