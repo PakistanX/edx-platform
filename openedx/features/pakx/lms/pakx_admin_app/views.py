c8f2f2762bc8fb8c2635cf7aa322e01faa8306e0
Faizan Shah <faizan.shah@edly.io>
2021-07-08 19:54:36 +0500

    Return total user count along with every query

--- openedx/features/pakx/lms/pakx_admin_app/views.py
+++ openedx/features/pakx/lms/pakx_admin_app/views.py
@@ -10,7 +10,7 @@ from django.http import Http404
 from django.middleware import csrf
 from django.utils.decorators import method_decorator
 from rest_framework import generics, status, views, viewsets
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.filters import OrderingFilter
 from rest_framework.response import Response
 
@@ -105,7 +105,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """
     User view-set for user listing/create/update/active/de-active
     """
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [SessionAuthentication]
     permission_classes = [CanAccessPakXAdminPanel]
     pagination_class = PakxAdminAppPagination
     serializer_class = UserSerializer
