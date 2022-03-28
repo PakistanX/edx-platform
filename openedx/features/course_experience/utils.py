73ebfcaa4d8335a9de9dddb95a8a25db35b98661
Zubair <36886070+emzubair@users.noreply.github.com>
2022-03-28 16:49:43 +0500

    PKX-916 VideoQuiz XBlock added in Progress traking list (#301)
    Co-authored-by: zubair-pkx <muhammad.zubair@arbisoft.com>

--- openedx/features/course_experience/utils.py
+++ openedx/features/course_experience/utils.py
@@ -208,6 +208,7 @@ def get_course_outline_block_tree(request, course_id, user=None, allow_start_dat
         'pakx_feedback',
         'pakx_video',
         'pakx_grid_dropdown',
+        'pakx_video_quiz',
         'pakx_completion'
     ]
     all_blocks = get_blocks(
