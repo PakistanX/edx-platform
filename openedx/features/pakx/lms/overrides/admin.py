b7a75c463cd1b8b1cf7220b679a55c62acab165b
zubair-pkx <muhammad.zubair@arbisoft.com>
2022-03-31 11:57:53 +0500

    PKX-949 Download as CSV action added for Course Progress Stats

--- openedx/features/pakx/lms/overrides/admin.py
+++ openedx/features/pakx/lms/overrides/admin.py
@@ -1,9 +1,37 @@
 """Admin panel for Completion API and Progress Stats"""
+from csv import writer as csv_writer
+from datetime import datetime
 
 from completion.models import BlockCompletion
 from django.contrib import admin
+from django.http import HttpResponse
 
 from openedx.features.pakx.lms.overrides.models import ContactUs, CourseProgressStats
+
+
+def download_as_csv(model_admin, request, queryset):    # pylint: disable=unused-argument
+    opts = model_admin.model._meta  # pylint: disable=protected-access
+    content_disposition = 'attachment; filename={}.csv'.format(opts.verbose_name)
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv_writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+download_as_csv.short_description = 'Download as CSV'
 
 
 class BlockCompletionAdmin(admin.ModelAdmin):
@@ -34,6 +62,7 @@ class CourseProgressStatsAdmin(admin.ModelAdmin):
     search_fields = ['email_reminder_status', 'progress']
     list_filter = ['email_reminder_status', 'progress', 'enrollment__user__profile__organization',
                    'enrollment__course__display_name']
+    actions = [download_as_csv]
 
     @staticmethod
     def user_email(obj):
