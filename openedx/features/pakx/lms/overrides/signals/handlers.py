c55a13cee206409e406a2d9fd3b71834f9fb30d3
Muhammad Usman <91720383+m-usman01@users.noreply.github.com>
2022-08-31 17:16:39 +0500

    PKX-1139 Changed enrolment modes for users enrolled in verified only courses through admin panel (#342)

--- openedx/features/pakx/lms/overrides/signals/handlers.py
+++ openedx/features/pakx/lms/overrides/signals/handlers.py
@@ -4,16 +4,14 @@ Progress Email related signal handlers.
 
 from django.dispatch import receiver
 
-from course_modes.models import CourseMode
 from lms.djangoapps.verify_student.models import ManualVerification
-from openedx.features.pakx.lms.overrides.tasks import add_enrollment_record, remove_enrollment_record
+from openedx.features.pakx.lms.overrides.tasks import (
+    add_enrollment_record,
+    remove_enrollment_record,
+    verify_user_and_change_enrollment
+)
 from student.models import EnrollStatusChange
 from student.signals import ENROLL_STATUS_CHANGE
-
-
-def course_is_verified(course_key):
-    """Check if course has a verified track."""
-    return bool(CourseMode.objects.filter(course_id=course_key, mode_slug=CourseMode.VERIFIED))
 
 
 def user_already_verified(user):
@@ -30,12 +28,6 @@ def copy_active_course_enrollment(sender, event=None, user=None, **kwargs):  # p
     course_key = str(kwargs.get('course_id', "Null"))
     if event == EnrollStatusChange.enroll:
         add_enrollment_record(user.id, course_key)
-        if course_is_verified(course_key) and user_already_verified(user) is False:
-            ManualVerification.objects.create(
-                status=u'approved',
-                user=user,
-                name='{}:{}'.format(user.id, course_key),
-                reason='Verified after enrolling in {}'.format(course_key)
-            )
+        verify_user_and_change_enrollment(user, course_key)
     elif event == EnrollStatusChange.unenroll:
         remove_enrollment_record(user.id, course_key)
