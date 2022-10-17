0d8b534e5e0107a7e43d9dae38b43948def11bc7
Muhammad Usman <91720383+m-usman01@users.noreply.github.com>
2022-10-17 14:39:54 +0500

    PKX-1176 Fixed mode not changing on re-enrolment of users (#352)
    (cherry picked from commit 14a30f30d88a8c3d6046f51ccdb888b9e3978c46)

--- openedx/features/pakx/lms/overrides/signals/handlers.py
+++ openedx/features/pakx/lms/overrides/signals/handlers.py
@@ -4,7 +4,6 @@ Progress Email related signal handlers.
 
 from django.dispatch import receiver
 
-from lms.djangoapps.verify_student.models import ManualVerification
 from openedx.features.pakx.lms.overrides.tasks import (
     add_enrollment_record,
     remove_enrollment_record,
@@ -14,17 +13,11 @@ from student.models import EnrollStatusChange
 from student.signals import ENROLL_STATUS_CHANGE
 
 
-def user_already_verified(user):
-    """Check if the user is already verified."""
-    return bool(ManualVerification.objects.filter(user=user))
-
-
 @receiver(ENROLL_STATUS_CHANGE)
 def copy_active_course_enrollment(sender, event=None, user=None, **kwargs):  # pylint: disable=unused-argument
     """
     Awards enrollment badge to the given user on new enrollments.
     """
-
     course_key = str(kwargs.get('course_id', "Null"))
     if event == EnrollStatusChange.enroll:
         add_enrollment_record(user.id, course_key)
