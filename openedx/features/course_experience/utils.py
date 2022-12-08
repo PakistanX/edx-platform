d32b88ca859fd293a1fe59a4447ab4df1b92dd5b
Rehan Farooq <103036740+rhn2@users.noreply.github.com>
2022-12-08 14:34:33 +0500

    Add configuration for Journal Xblock (#359)
    (cherry picked from commit 4da5e25a9e166a5b356eb989077265798033d0c9)

--- openedx/features/course_experience/utils.py
+++ openedx/features/course_experience/utils.py
@@ -211,7 +211,8 @@ def get_course_outline_block_tree(request, course_id, user=None, allow_start_dat
         'pakx_video_quiz',
         'pakx_microlearning',
         'pakx_completion',
-        'openassessment'
+        'openassessment',
+        'journal_xblock'
     ]
     all_blocks = get_blocks(
         request,
