03cccef26d37522b0e105022f1c330f60ea1b85a
Muhammad Usman <91720383+m-usman01@users.noreply.github.com>
2023-01-06 13:15:11 +0500

    PKX-1199 Changed redirection approach (#362)

--- openedx/features/pakx/lms/overrides/views.py
+++ openedx/features/pakx/lms/overrides/views.py
@@ -1,6 +1,5 @@
 """ Overridden views from core """
 from datetime import datetime
-from urllib.parse import unquote
 
 from django.conf import settings
 from django.contrib import messages
@@ -253,11 +252,13 @@ def _get_course_about_context(request, course_id, category=None):  # pylint: dis
     def _get_ecommerce_data(mode):
         single_link = ''
         bulk_link = ''
+        mode_sku = ''
         if mode and mode.sku:
             single_link = ecomm_service.get_checkout_page_url(mode.sku)
+            mode_sku = mode.sku
         if mode and mode.bulk_sku:
             bulk_link = ecomm_service.get_checkout_page_url(mode.bulk_sku)
-        return single_link, bulk_link
+        return single_link, bulk_link, mode_sku
 
     course_key = CourseKey.from_string(course_id)
 
@@ -299,6 +300,7 @@ def _get_course_about_context(request, course_id, category=None):  # pylint: dis
         ecommerce_checkout = ecomm_service.is_enabled(request.user)
         ecommerce_checkout_link = ''
         ecommerce_bulk_checkout_link = ''
+        sku = ''
         single_paid_mode = None
         upgrade_data = None
         if ecommerce_checkout:
@@ -309,9 +311,9 @@ def _get_course_about_context(request, course_id, category=None):  # pylint: dis
 
             if not single_paid_mode:
                 upgrade_data = modes.get('verified')
-                ecommerce_checkout_link, ecommerce_bulk_checkout_link = _get_ecommerce_data(upgrade_data)
+                ecommerce_checkout_link, ecommerce_bulk_checkout_link, sku = _get_ecommerce_data(upgrade_data)
             else:
-                ecommerce_checkout_link, ecommerce_bulk_checkout_link = _get_ecommerce_data(single_paid_mode)
+                ecommerce_checkout_link, ecommerce_bulk_checkout_link, sku = _get_ecommerce_data(single_paid_mode)
 
         _, course_price = get_course_prices(course, for_about_page=True)
 
@@ -385,6 +387,7 @@ def _get_course_about_context(request, course_id, category=None):  # pylint: dis
             'ecommerce_checkout': ecommerce_checkout,
             'ecommerce_checkout_link': ecommerce_checkout_link,
             'ecommerce_bulk_checkout_link': ecommerce_bulk_checkout_link,
+            'sku': sku,
             'single_paid_mode': single_paid_mode,
             'upgrade_data': upgrade_data,
             'show_upgrade_after_enrollment': show_upgrade_after_enrollment,
@@ -780,13 +783,13 @@ def course_about_static(request):
 @login_required
 @ensure_csrf_cookie
 @require_http_methods(["GET"])
-def basket_check(request, course_key_string, ne):
+def basket_check(request, course_key_string, sku):
     """Check if user is already enrolled in course.
 
     Open edX checks if the user is already enrolled in course through orders on the ecommerce site. Since we are
     manually enrolling users as well, we need to check if user is already enrolled or not.
     """
-    redirect_url = unquote(ne)
+    redirect_url = '{}/basket/add/?sku={}'.format(settings.ECOMMERCE_PUBLIC_URL_ROOT, sku)
     course_enrollment = CourseEnrollment.get_enrollment(user=request.user, course_key=course_key_string)
     if course_enrollment is None:
         return redirect(redirect_url)
