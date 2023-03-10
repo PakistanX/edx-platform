1c4182894b6ccaea8f32d360c810081c7915760d
Muhammad Usman <91720383+m-usman01@users.noreply.github.com>
2023-03-10 12:49:57 +0500

    PKX-1286 Functional changes for CAP high touch (#376)

--- openedx/features/pakx/lms/discover/urls.py
+++ openedx/features/pakx/lms/discover/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
 from django.urls import path
 
-from openedx.features.pakx.lms.discover.views import BusinessCoursesView, CoursesListView, UserProfileImage
+from openedx.features.pakx.lms.discover.views import (
+    BusinessCoursesView,
+    CourseAboutPageData,
+    CoursesListView,
+    UserProfileImage
+)
 
 urlpatterns = [
     url(r'^courses_data/$', CoursesListView.as_view(), name='course-data-api'),
     url(r'^business_courses_data/$', BusinessCoursesView.as_view(), name='business-data-api'),
+    url(r'^course_about_data/', CourseAboutPageData.as_view(), name='course-about-high-touch'),
     path('profile_image/<str:username>', UserProfileImage.as_view(), name='user-profile-image'),
 ]
