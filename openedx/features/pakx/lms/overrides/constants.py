c1cc96d7bf1adbfe416b44bf1ebafa6c8827be18
Ahmad Shafique <ahmadxhafique@gmail.com>
2024-05-15 15:04:55 +0500

    enable lums certificate for happy place 5e course

--- openedx/features/pakx/lms/overrides/constants.py
+++ openedx/features/pakx/lms/overrides/constants.py
@@ -3,6 +3,7 @@ TEACHING_PRACTICE_COURSE_IDS = [
     'course-v1:Lecole_AES+2+2023',
     'course-v1:LGS_JT+2+2022',
     'course-v1:Jadid_Dastgir+2+2022',
+    'course-v1:Happy_Palace_Group_of_Schools.+2+2024',
 ]
 
 LUMS_CERTIFICATE_ORGANIZATIONS = [
